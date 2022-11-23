fd6e0ca48f49687a2768332797f312fb0e931f3e
Alexis <yernauxalex@gmail.com>
2022-11-23 10:24:26 +0100

    Add error message + some comment

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,12 +16,13 @@ type Data = {
 
 const Main = () => {
     const [selectedDate, setSelectedDate] = useState<Date>(new Date());
+    const [displayError, setDisplayError] = useState<string>("");
     //state with the data from the api
     const [data, setData] = useState<Data[]>();
 
     const onSubmit = async (e: FormEvent) => {
         e.preventDefault();
-        // Reset the data state
+        // Reset the data state in case the user submit a new date
         setData(undefined);
         // convert date to UTC
         const utcDate: Date = new Date(selectedDate.toUTCString());
@@ -30,27 +31,29 @@ const Main = () => {
 
         await getFlows(flow_id).then((response) => {
             if (response === null) {
-                return alert("Flow id not found");
+                return setDisplayError("Flow id not found");
             }
 
             getRuns(flow_id, isoDate).then((response) => {
                 if (response === null) {
-                    return alert("no run available for this date");
+                    return setDisplayError("No runs found for this date");
                 }
                 if (response.results[0].complete === false) {
-                    return alert("no complete run for this date");
+                    return setDisplayError("Run not complete for this date");
                 }
                 const run_id = response.results[0].id;
 
                 getOutputs(flow_id, run_id).then((response) => {
                     if (response === null) {
-                        return alert("No outputs found");
+                        return setDisplayError("No outputs found for this run");
                     }
                     const output_id = response.results[0].id;
 
                     getTrends(flow_id, run_id, output_id).then((response) => {
                         if (response === null) {
-                            return alert("No trends found");
+                            return setDisplayError(
+                                "No trends found for this output"
+                            );
                         }
 
                         // Create the dataset who will be used to display the table
@@ -63,6 +66,8 @@ const Main = () => {
                                 trend: item.trend,
                             });
                         });
+                        // reset the error message
+                        setDisplayError("");
                         setData(data);
                     });
                 });
@@ -83,6 +88,11 @@ const Main = () => {
                     <Col md={3} xs="auto">
                         <Form.Control
                             type="date"
+                            required
+                            //add default value to the input
+                            defaultValue={selectedDate
+                                .toISOString()
+                                .slice(0, 10)}
                             value={selectedDate.toISOString().slice(0, 10)}
                             onChange={(e) =>
                                 setSelectedDate(new Date(e.target.value))
@@ -99,6 +109,10 @@ const Main = () => {
                         </Button>
                     </Col>
                 </Form.Group>
+                {/* display error message */}
+                {displayError && (
+                    <p className="text-danger text-center">{displayError}</p>
+                )}
             </Form>
             {data && <Result data={data} />}
         </Container>
